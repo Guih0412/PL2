0ed64d22eb6073188feba5a1e316f4ee35b02524
Guilherme Henrique <163424935+Guih0412@users.noreply.github.com>
2025-06-21 19:07:44 -0300

    Substituindo funçao por classe no route

--- src/components/roteador.tsx
+++ src/components/roteador.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Footer from "./footer";
 import Cliente from "../pages/cliente";
 import Pet from "../pages/pet";
@@ -10,30 +11,37 @@ import Servico from "../pages/servico";
 import Header from "./header";
 import Relatorio from "../pages/relatorio";
 
+type State = {};
 
-export default function Roteador() {
-    const navigate = useNavigate();
+export default class Roteador extends Component<{}, State> {
+  constructor(props: {}) {
+    super(props);
+    this.selecionarView = this.selecionarView.bind(this);
+  }
 
-    function selecionarView(view: string) {
-        navigate(`/${view.toLowerCase()}`);
-    }
+  selecionarView(view: string) {
+  
+    window.location.href = `/${view.toLowerCase()}`;
+  }
 
-    const botoes = ['Home', 'Cliente', 'Pet', 'Produto', 'Servico','Relatorio'];
+  render() {
+    const botoes = ['Home', 'Cliente', 'Pet', 'Produto', 'Servico', 'Relatorio'];
+    const tema = "rgb(255, 123, 0)";
 
     return (
-        <>
-            <Header seletorView={selecionarView} tema="rgb(255, 123, 0)" botoes={botoes} />
-            <Routes>
-                <Route path="/" element={<Home tema="rgb(255, 123, 0)" />} />
-                <Route path="/home" element={<Home tema="rgb(255, 123, 0)" />} />
-                <Route path="/cliente" element={<Cliente />} />
-                <Route path="/pet" element={<Pet/>}/>
-                <Route path="/produto" element={<Produto/>}/>
-                <Route path="/servico" element={<Servico/>}/>
-                <Route path="/relatorio" element={<Relatorio/>}/>
-            </Routes>
-
-            <Footer botoes={botoes} tema="rgb(255,123,0)"/>
-        </>
+      <>
+        <Header seletorView={this.selecionarView} tema={tema} botoes={botoes} />
+        <Routes>
+          <Route path="/" element={<Home tema={tema} />} />
+          <Route path="/home" element={<Home tema={tema} />} />
+          <Route path="/cliente" element={<Cliente />} />
+          <Route path="/pet" element={<Pet />} />
+          <Route path="/produto" element={<Produto />} />
+          <Route path="/servico" element={<Servico />} />
+          <Route path="/relatorio" element={<Relatorio />} />
+        </Routes>
+        <Footer botoes={botoes} tema={tema} />
+      </>
     );
+  }
 }
